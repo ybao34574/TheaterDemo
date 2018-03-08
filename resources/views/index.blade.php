0d0d04798aa8d9e29fda8627b2054a80d2143379
Yana Bao <byn_softbank@126.com>
2018-03-08 10:57:27 +0800

    add weixin and zhifubao button

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,17 +13,17 @@
             <h1 style="">研发中心笔试题-某公司晚会购票DEMO系统</h1>
         </div>
         <div class="panel-body">
-            <form action="/" role="form" style="margin-left: 40%;margin-right: 40%" method="POST">
+            <form action="/" role="form" method="POST">
                 <input type="hidden" name="_token" value="{{ csrf_token() }}" />
-                <div class="form-group">
+                <div class="form-group col-md-4">
                     <label for="email">姓名</label>
                     <input class="form-control" type="text" name="name" required>
                 </div>
-                <div class="form-group">
+                <div class="form-group col-md-4">
                     <label for="email">邮箱</label>
                     <input class="form-control" type="email" name="email" required>
                 </div>
-                <div class="form-group">
+                <div class="form-group col-md-4">
                     <label for="ticket">预购买票数量</label>
                     <select class="form-control" name="tickets" required>
                         <option value="1">1</option>
@@ -34,7 +34,9 @@
                     </select>
                 </div>
                 <button type="submit" class="btn btn-default" name="search" value="search">查询我的晚会票</button>
-                <button type="submit" class="btn btn-default" name="submit" value="submit">购买</button>
+                <button type="submit" class="btn btn-default" name="submit" value="submit">假设支付完成下一步</button>
+                <button type="submit" class="btn btn-default" name="wexin" value="submit" disabled>微信支付</button>
+                <button type="submit" class="btn btn-default" name="zhifubao" value="submit" disabled>支付宝</button>
             </form>
         </div>
     </div>
